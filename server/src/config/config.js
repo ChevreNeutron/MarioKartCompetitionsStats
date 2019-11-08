7d9da726e611a5e184a8511a1a2d3ad793f48e95
ChevreNeutron <lucas.hervouet974@gmail.com>
2019-11-08 18:07:59 +0100

    [ADD] Registration service ok

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    port: process.env.PORT || 8081,
+    db: {
+        database: process.env.DB_NAME || 'mariokartcompetitionstats',
+        user: process.env.DB_USER || 'mariokartcompetitionstats',
+        password: process.env.DB_PASS || 'mariokartcompetitionstats',
+        options: {
+            dialect: process.env.DIALECT ||'sqlite',
+            host: process.env.HOST || 'localhost',
+            storage: './mariokartcompetitionstats.sqlite'
+        }
+    }
+}
