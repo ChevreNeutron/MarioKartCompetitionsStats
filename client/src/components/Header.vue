ef2e5651afa65b62c7c9c57b0416e8341335e8c9
ChevreNeutron <lucas.hervouet974@gmail.com>
2020-02-16 19:30:57 +0100

    [ADD] Logout management done

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -32,19 +32,32 @@
             Sign Up
           </router-link>
         </v-btn>
-<!--
-        <v-btn class="red darken-2"
+
+        <v-btn v-if="$store.state.isUserLoggedIn"
+          class="red darken-2"
           flat
           dark
           @click="logout">
           Log Out
-        </v-btn>-->
+        </v-btn>
       </v-toolbar-items>
     </v-app-bar>
 </template>
 
 <script>
 export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    },
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'root'
+      })
+    }
+  }
 }
 </script>
 
