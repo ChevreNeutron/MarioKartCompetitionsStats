c09b3f36de36d0c67e4fc8d22d830f74379e05bc
ChevreNeutron <lucas.hervouet974@gmail.com>
2020-02-23 17:39:39 +0100

    [ADD] Panel component OK

--- client/src/components/Characters.vue
+++ client/src/components/Characters.vue
@@ -1,15 +1,35 @@
 <template>
-  <v-layout>
-    <v-flex xs6 offset-xs3>
-      <br>
-        Register
-    </v-flex>
-  </v-layout>
+    <v-layout column>
+        <v-flex>
+            <panel title="Characters">
+                <div 
+                    v-for="character in characters" 
+                    :key="character.name">
+                    {{character.name}}
+                    {{character.image}}
+                </div>
+            </panel>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
 
+import Panel from '@/components/Panel'
 export default {
+    components: {
+        Panel
+    },
+    data () {
+        return {
+            characters: [
+                {
+                    name: 'Mario',
+                    image: 'Test.jpeg'
+                }
+            ]
+        }
+    }
 }
 </script>
 
---
+++ client/src/components/Panel.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="white elevation-2">
+        <v-toolbar flat dense class="red" dark>
+            <v-toolbar-title align="center" justify="center">{{title}}</v-toolbar-title>
+        </v-toolbar>
+        
+        <div>
+            <slot>
+                No slot content defined.
+            </slot>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: [
+        'title'
+    ]
+}
+</script>
+
+<!-- -->
+<style scoped>
+</style>
